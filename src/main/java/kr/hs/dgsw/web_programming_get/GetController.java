f09d44f9f6cd8ca978dadb5a2873dd1577f2cdb6
vhrleo0904 <30452958+vhrleo0904@users.noreply.github.com>
2019-03-12 18:00:04 +0900

    과제 완료

---
+++ src/main/java/kr/hs/dgsw/web_programming_get/GetController.java
@@ -0,0 +1,26 @@
+package kr.hs.dgsw.web_programming_get;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class GetController {
+
+    @GetMapping("/greeting")
+    public String sayHello(@RequestParam(required = false, defaultValue = "Hello") String say,
+                           @RequestParam(required = false, defaultValue = "NONAME") String to) {
+        return say + " " + to;
+    }
+
+    @GetMapping(value = {"/greeting1/{say}/{to}", "/greeting1/{say}", "/greeting1//{to}", "/greeting1"})
+    public String sayHello1(@PathVariable Optional<String> say, @PathVariable Optional<String> to) {
+        String temp = say.isPresent() ? say.get() : "Hello";
+        String tmp = to.isPresent() ? to.get() : "NONAME";
+
+        return temp + " " + tmp;
+    }
+}
---
+++ src/main/java/kr/hs/dgsw/web_programming_get/WebProgrammingGetApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.web_programming_get;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgrammingGetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProgrammingGetApplication.class, args);
+    }
+
+}
